21bdf958252401ee9d338db1bbf5fae43004aab2
Kzzz1st <2309145033@qq.com>
2021-03-22 13:34:44 +0800

    code

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    int n;
+    while(scanf("%d",&n)!=EOF)
+    {
+        int a[100004]={0};
+        for(int i=0;i<n;i++)
+            scanf("%d",&a[i]);
+            int c1=0,ma=-1,c2=0;
+        for(int i=0;i<n;i++)
+        {
+           for(int j=0;j<n;j++)
+           {
+
+           }
+        }
+        printf("%d\n",2*ma);
+    }
+    return 0;
+}
